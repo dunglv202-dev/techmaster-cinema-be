60de9c699e7f0d953aec1ded242f3182deedd20b
dunglv202 <dunglv202@gmail.com>
2024-10-09 11:21:03 +0700

    initial commit

---
+++ src/main/java/dev/dunglv202/techmaster/TechmasterApplication.java
@@ -0,0 +1,13 @@
+package dev.dunglv202.techmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dunglv202/techmaster/TechmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dunglv202.techmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

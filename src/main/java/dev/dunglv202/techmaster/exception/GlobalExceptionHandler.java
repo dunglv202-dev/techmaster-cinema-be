33359e57155184dd7016feae25c6538774bd9089
dunglv202 <dunglv202@gmail.com>
2024-10-19 15:58:54 +0700

    handle exception message

--- src/main/java/dev/dunglv202/techmaster/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/dunglv202/techmaster/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package dev.dunglv202.techmaster.exception;
 
 import dev.dunglv202.techmaster.dto.ApiError;
 import dev.dunglv202.techmaster.dto.ApiResp;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +17,20 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResp<?> validationError(MethodArgumentNotValidException e) {
+        assert e.getBindingResult().getFieldError() != null;
+        return ApiError.code(400).message(e.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResp<?> handleConstrainViolation(ConstraintViolationException e) {
+        String err = e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).toList().get(0);
+        return ApiError.code(400).message(err);
+    }
+
     @ExceptionHandler(ClientVisibleException.class)
     public ResponseEntity<?> visibleException(ClientVisibleException e) {
         return ResponseEntity.status(e.getStatus()).body(ApiError.code(e.getCode()).message(e.getMessage()));
